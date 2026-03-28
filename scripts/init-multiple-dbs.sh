49b067126936b86ce32c9d0d145a482af6b35ca8
Aniketh Mahadik <anikethani32@gmail.com>
2026-03-28 21:38:34 +0530

    fix(infra): connect init script to POSTGRES_DB and remap postgres port to 5433

--- scripts/init-multiple-dbs.sh
+++ scripts/init-multiple-dbs.sh
@@ -18,7 +18,7 @@ create_user_and_database() {
     local database=$1
     echo "Creating database '$database' owned by '$POSTGRES_USER'"
 
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         CREATE DATABASE $database OWNER $POSTGRES_USER;
         GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
